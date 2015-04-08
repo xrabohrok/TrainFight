9fe2f7eac0e353f0766c28e0a62b25c28c9cd809
westin <westin007@gmail.com>
2015-04-07 20:25:29 -0500

    Started making a thing that will run between zones - Mr.Black

---
+++ Assets/ZoneKeeper/Zone.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.ZoneKeeper
+{
+    public class Zone : MonoBehaviour
+    {
+        public List<Zone> Neighbors;
+        public List<ZoneFollower> Guards;
+        public int MaxOccupants = 1;
+
+
+
+        // Use this for initialization
+        public void Start () {
+	        
+        }
+	
+        // Update is called once per frame
+        public void Update () {
+	
+        }
+
+        public Vector3? WhereAmI(ZoneFollower immigrant)
+        {
+            if (Guards.Count < MaxOccupants)
+            {
+                Guards.Add(immigrant);
+
+                //TODO: make this a derived location instead
+                return this.gameObject.transform.position;
+            }
+            return null;
+        }
+    }
+}
---
+++ Assets/ZoneKeeper/ZoneFollower.cs
@@ -0,0 +1,41 @@
+﻿using Assets.HotSpot;
+using UnityEngine;
+
+namespace Assets.ZoneKeeper
+{
+    [RequireComponent(typeof(Clickable))]
+    public class ZoneFollower : MonoBehaviour
+    {
+
+        public GameObject CurrentZone;
+        public float DistanceVariations = 1f;
+
+        private Zone _attachedZone;
+        private Clickable _clickable;
+
+        // Use this for initialization
+        public void Start ()
+        {
+            if(CurrentZone != null)
+            {
+                _attachedZone = CurrentZone.GetComponent<Zone>();
+                if (_attachedZone == null)
+                    CurrentZone = null;
+            }
+            _clickable = GetComponent<Clickable>();
+        }
+	
+        // Update is called once per frame
+        public void Update ()
+        {
+            var here = this.gameObject.transform.position;
+            var position = _attachedZone.gameObject.transform.position;
+            var distance = Vector3.Distance(here, position);
+            if (distance > DistanceVariations)
+            {
+                var lookAtPost = new Vector3(position.x, here.y, position.z);
+                this.gameObject.transform.LookAt(lookAtPost,Vector3.up);
+            }
+        }
+    }
+}

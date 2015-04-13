9eef205c2cf0d253ddec708b4a25d4a6e6da8d92
Mr. Black <westin007@gmail.com>
2015-04-12 22:36:49 -0500

    Beginning of Soldier master - Mr. Black

---
+++ Assets/Soldiers/SoldierMaster.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Code.Clickable;
+using UnityEngine;
+
+namespace Assets.Soldiers
+{
+    [RequireComponent(typeof(ClickEventDelegator))]
+    public class SoldierMaster : MonoBehaviour {
+        private ClickEventDelegator _slaveDriver;
+
+        // Use this for initialization
+        void Start () {
+
+            _slaveDriver = this.gameObject.GetComponent<ClickEventDelegator>();
+            if (_slaveDriver == null)
+                throw new System.NullReferenceException();
+
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}
